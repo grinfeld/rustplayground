cee8843809f94549c9b7a17968aef980d2797977
michael.grinfeld <michael.grinfeld@dynamicyield.com>
2023-11-19 15:52:08 +0200

    posix to postfix

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,32 @@ use std::collections::HashSet;
 fn main() {
     // stack::test_reverse();
 
-    let ops : HashSet<Element> = HashSet::from([PLUS, MINUS, MUL, DIV]);
-    let ns = String::from("1+ 3 - 1".trim());
-    let mut st: Vec<Element> = stack::new_stack(ns.len());
+    let ops : HashSet<Element> = HashSet::from([PLUS, MINUS, MUL, DIV, POW, OPEN_BRACKET, CLOSE_BRACKET]);
+    let op_chars: HashSet<char> = HashSet::from(['+', '-', '*', '/', '(', ')', '^']);
+    let ns = String::from("(33 + 45 / 3 *(2+9)-50)".trim());
+    let mut operators: Vec<Element> = stack::new_stack(ns.len());
+    let mut numbers: Vec<Element> = stack::new_stack(ns.len());
 
     // prev sign, could be 0
     let mut prev_char: char = ' ';
-    let mut prev_op = FIRST;
+    let mut prev_op = EMPTY_OP;
     let mut num: String = String::from("");
 
     for c in ns.chars() {
         if c.is_numeric() {
             num = num + &*String::from(c);
             prev_char = c;
-        } else if c != EMPTY_CHAR { // means not empty and not
-            let n: u32 = num.parse().unwrap();
-            num = String::from("");
-
-            let op : Element = Element::Operation(c);
-            if ops.contains(&prev_op) || prev_op == FIRST {
-                set(&mut st, op, Element::Number(n), prev_op);
+        } else if op_chars.contains(&c) { // means not empty and not
+            if !num.is_empty() {
+                let n: u32 = num.parse().unwrap();
+                num = String::from("");
+                numbers.push(Element::Number(n));
             }
+            let op : Element = Element::Operation(c);
+            if ops.contains(&prev_op) {
+                operators.push(prev_op);
+            }
+            prev_op = op;
 
             match op {
                 PLUS => prev_op = PLUS,
@@ -35,22 +40,23 @@ fn main() {
             }
         }
     }
-    set(&mut st, prev_op, Element::Number(num.parse().unwrap()), prev_op);
-
-    stack::print_stack(&st, |op: &Element| op.to_string())
-}
-
-fn set(st: &mut Vec<Element>, current_op: Element, n: Element, mut prev_op: Element) {
-    st.push(n);
-    if prev_op != FIRST {
-        st.push(prev_op);
+    if !num.is_empty() {
+        let n: u32 = num.parse().unwrap();
+        numbers.push(Element::Number(n));
     }
-    prev_op = current_op
+
+    if prev_op != EMPTY_OP {
+        operators.push(prev_op);
+    }
+    //set(&mut operators, prev_op, Element::Number(num.parse().unwrap()), prev_op);
+
+    stack::print_stack(&operators, |op: &Element| op.to_string());
+    stack::print_stack(&numbers, |op: &Element| op.to_string());
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Element {
-    Operation(char),
+    Operation { character: char, order: i32 },
     Number(u32)
 }
 
@@ -58,21 +64,20 @@ impl Element {
     fn to_string(&self) -> String {
         match self {
             Element::Number(n) => n.to_string(),
-            Element::Operation(c) => c.to_string()
+            Element::Operation { character, order} => character.to_string() + "(" + &order.to_string() + ")"
         }
     }
 }
 
 
-const EMPTY_CHAR: char = ' ';
+const EMPTY_CHAR: char = '\0';
 
-const PLUS : Element =  Element::Operation('+');
-const MINUS : Element =  Element::Operation('-');
-const MUL : Element =  Element::Operation('*');
-const DIV : Element =  Element::Operation('/');
-const FIRST : Element =  Element::Operation('_');
+const PLUS : Element =  Element::Operation{character: '+', order: 0};
+const MINUS : Element =  Element::Operation{character: '-', order: 0};
+const MUL : Element =  Element::Operation{character: '*', order: 1};
+const DIV : Element =  Element::Operation{character: '/', order: 1};
+const POW : Element =  Element::Operation{character: '^', order: 2};
+const OPEN_BRACKET : Element =  Element::Operation{character: '(', order: 3};
+const CLOSE_BRACKET : Element =  Element::Operation{character: ')', order: 3};
 
-const OPEN_BRACKET : Element =  Element::Operation('(');
-const CLOSE_BRACKET : Element =  Element::Operation(')');
-
-const EMPTY_OP: Element =  Element::Operation(' ');
+const EMPTY_OP: Element =  Element::Operation{character: EMPTY_CHAR, order: -1};
